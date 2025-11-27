22602bf61bc4c3ae35eaa4ac4ec6149a50e94767
nihalmohammed18 <nihalm@omnistrate.com>
2025-11-27 15:15:28 +0530

    Fix

--- app/posts/[slug]/components/case-study-components/section-title.tsx
+++
@@ -1,5 +0,0 @@
-import { DisplaySM } from "@/components/text";
-
-export const SectionTitle = ({ children }: { children: React.ReactNode }) => {
-  return <DisplaySM className="mt-3 mb-5">{children}</DisplaySM>;
-};
--- app/posts/[slug]/components/case-study-components/section.tsx
+++
@@ -1,3 +0,0 @@
-export const Section = ({ children }: { children: React.ReactNode }) => {
-  return <section className="my-12 md:my-16">{children}</section>;
-};
